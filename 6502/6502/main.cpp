51abd4f913fec73681077377faa0ecc663f31071
eykamt <embergameslp@gmail.com>
2023-09-17 18:07:18 -0400

    created execute function

--- 6502/6502/main.cpp
+++ 6502/6502/main.cpp
@@ -1,6 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+using BYTE = unsigned char;
+using WORD = unsigned short;
+using u32 = unsigned int;
+
+struct Memory{
+	static constexpr u32 MAX_MEM = 1024 * 64;
+	BYTE data[MAX_MEM];
+
+	void initialize() {
+		for (u32 i = 0; i < MAX_MEM; i++) {
+			data[i] = 0;
+		};
+	};
+
+	BYTE operator[](u32 address) const {
+		return data[address];
+	};
+};
+
+struct CPU {
+
+	WORD PC; // program counter
+	WORD SP; // stack pointer
+
+	BYTE A, X, Y; // registers
+
+	BYTE C : 1; // carry flag
+	BYTE Z : 1; // zero flag
+	BYTE I : 1; // interrupt disable
+	BYTE D : 1; // decimal mode
+	BYTE B : 1; // break command
+	BYTE V : 1; // overflow flag
+	BYTE N : 1; // negative flag
+
+	void reset(Memory& mem) {
+		PC = 0xFFFC;
+		SP = 0x0100;
+		C = Z = I = D = B = V = N = 0;
+		A = X = Y = 0;
+		mem.initialize();
+	};
+
+	BYTE fetch(u32& cycles, Memory& mem) {
+		BYTE data = mem[PC];
+		PC++;
+		cycles--;
+		return data;
+	};
+
+	void execute(u32 cycles, Memory& mem) {
+		while (cycles > 0) {
+			BYTE insion = fetch(cycles, mem);
+			(void)insion;
+		};
+	};
+};
+
 int main() {
+	Memory mem;
+	CPU cpu;
+	cpu.reset(mem);
+	cpu.execute(2, mem);
 	return 0;
 };
