e1e6f3bdee10262fb40407e31b86dc28dde53ca9
eykamt <embergameslp@gmail.com>
2023-09-19 21:12:41 -0400

    aded inline program

--- 6502/6502/main.cpp
+++ 6502/6502/main.cpp
@@ -15,7 +15,7 @@ struct Memory{
 		};
 	};
 
-	BYTE operator[](u32 address) const {
+	BYTE& operator[](u32 address) {
 		return data[address];
 	};
 };
@@ -50,10 +50,39 @@ struct CPU {
 		return data;
 	};
 
+	BYTE read(u32& cycles, BYTE address, Memory& mem) {
+		BYTE data = mem[address];
+		cycles--;
+		return data;
+	};
+
+	static constexpr BYTE
+		INS_LDA_IM = 0xA9,
+		INS_LDA_ZP = 0xA5;
+
+	void LDASetStatus() {
+		Z = (A == 0);
+		N = (A & 0b10000000) > 0;
+	};
+
 	void execute(u32 cycles, Memory& mem) {
 		while (cycles > 0) {
 			BYTE insion = fetch(cycles, mem);
-			(void)insion;
+			switch (insion) {
+				case INS_LDA_IM: {
+					BYTE value = fetch(cycles, mem);
+					A = value;
+					LDASetStatus();
+				} break;
+				case INS_LDA_ZP: {
+					BYTE zeroPageAddress = fetch(cycles, mem);
+					A = read(cycles, zeroPageAddress, mem);
+					LDASetStatus();
+				} break;
+				default: {
+					printf("Instruction not handled");
+				} break;
+			};
 		};
 	};
 };
@@ -62,6 +91,11 @@ int main() {
 	Memory mem;
 	CPU cpu;
 	cpu.reset(mem);
-	cpu.execute(2, mem);
+
+	mem[0xFFFC] = CPU::INS_LDA_ZP;
+	mem[0xFFFD] = 0x42;
+	mem[0x0042] = 0x84;
+
+	cpu.execute(3, mem);
 	return 0;
 };
